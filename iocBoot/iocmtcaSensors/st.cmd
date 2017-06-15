adf162d915c1626f48b12667fc7f2466df6685eb
Wayne Lewis <wlewis@ospreydcs.com>
2017-06-15 13:19:58 -0700

    Add IOC boilerplate

---
+++ iocBoot/iocmtcaSensors/st.cmd
@@ -0,0 +1,19 @@
+#!../../bin/linux-x86_64/mtcaSensors
+
+## You may have to change mtcaSensors to something else
+## everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/mtcaSensors.dbd"
+mtcaSensors_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+#dbLoadRecords("db/xxx.db","user=wayne")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
