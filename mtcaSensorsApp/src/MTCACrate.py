ca721293577a8254030c856c9b079514c968158d
Wayne Lewis <wlewis@ospreydcs.com>
2017-06-19 19:29:14 -0700

    WIP: setting up for slot-based method of getting sensor data

--- mtcaSensorsApp/src/MTCACrate.py
+++ mtcaSensorsApp/src/MTCACrate.py
@@ -7,10 +7,25 @@
 # FRU = Field Replaceable Unit
 #
 
-#from devsup.db import IOScanListBlock
-#from devsup.util import StoppableThread
-import threading
 from subprocess import check_output
+
+AMC_SLOT_1 = 97
+
+SENSOR_NAMES = {
+        '12 V PP': '12V',
+        '12 V AMC': '12V',
+        '3.3 V PP': '3V3',
+        '3.3 V MP': '3V3',
+        '1.8 V', '1V8',
+        '1.0 V CORE', '1V0'
+        }
+
+ALARMS = {
+        'Lower Critical': 'lolo',
+        'Lower Non-Critical': 'low',
+        'Upper Non-Critical': 'high',
+        'Upper Critical': 'hihi'
+        }
 
 #_crates = {}
 
@@ -30,6 +45,19 @@ def get_crate():
     except:
         pass
 
+class Sensor():
+    """
+    Sensor information
+    """
+    def __init__(self, name):
+        self.name = name
+        self.value = 0.0
+        self.lolo = 0.0
+        self.low = 0.0
+        self.high = 0.0
+        self.hihi = 0.0
+        self.alarms_set = False
+
 class FRU():
     """
     FRU information
@@ -42,12 +70,19 @@ class FRU():
         Args:
             id (str): FRU ID (e.g., 192.101)
             name (str): FRU name
+            slot(int): AMC slot number
+            crate(obj): reference to crate object
 
         Returns: 
             Nothing
         """
         self.id = id
         self.name = name
+        self.slot  = slot
+        self.crate = crate
+
+        # Properties for storing sensor values
+        self.sensors = {}
 
     def __str__(self):
         """
@@ -61,7 +96,88 @@ class FRU():
         """
         return "ID: {}, Name: {}".format(self.id, self.name)
 
+    def read_sensors(self):
+        """
+        Read the sensors for this FRU
 
+        Args:
+            None
+
+        Returns:
+            Nothing
+        """
+
+        # Create the IPMI tool command
+        command = []
+        command.append("ipmitool")
+        command.append("-H")
+        command.append(self.crate.host)
+        command.append("-U")
+        command.append(self.crate.user)
+        command.append("-P")
+        command.append(self.crate.password)
+        command.append("sdr")
+        command.append("entity")
+        command.append(self.id)
+
+        result = check_output(command)
+        
+        for line in result.splitlines():
+            try:
+                line_strip = [x.strip() for x in line.split('|')]
+                name, sensor_id, status, fru_id, val = line_strip
+                value, egu = val.split(' ', 1)
+                # Check if the sensor name is in the list we know about
+                if sensor_name in SENSOR_NAMES.keys():
+                    sensor_type = SENSOR_NAMES[name]
+                    # Check if we have already created this sensor
+                    if not sensor_type in self.sensors.keys():
+                        self.sensors[sensor_type] = Sensor(name)
+                    self.sensors[sensor_type].value = float(value)
+                    self.sensors[sensor_type].egu = egu
+
+                    if not self.sensors[sensor_type].alarms_set:
+                        self.set_alarms(sensor_name)
+                        self.set_alarms[sensor_type].alarms_set = True
+            except ValueError:
+                pass
+
+    def set_alarms(self, name):
+        """
+        Function to set alarm setpoints in AI records
+
+        Args:
+            name: sensor name
+
+        Returns:
+            Nothing
+        """
+        command = []
+        command.append("ipmitool")
+        command.append("-H")
+        command.append(self.crate.host)
+        command.append("-U")
+        command.append(self.crate.user)
+        command.append("-P")
+        command.append(self.crate.password)
+        command.append("sensor")
+        command.append("get")
+        command.append(name)
+
+        result = check_output(command)
+
+        for line in result.splitlines():
+            try:
+                item, value = [x.strip() for x in line.split(':',1)]
+                if item in ALARMS.keys():
+                    sensor_type = SENSOR_NAMES[name]
+                    setattr(self.sensors[sensor_type], ALARMS[item], float(value))
+            except ValueError:
+                pass
+
+        self.sensors[name].alarms_set = True       
+
+        
 class MTCACrate():
     """
     Class for holding microTCA crate information, including FRU list
@@ -82,8 +198,9 @@ class MTCACrate():
         self.user = None
         self.password = None
 
-        # Initialize list of FRUs
-        self.frus = []
+        # Initialize dict of FRUs
+        self.frus = {}
+        self.frus_inited = False
 
     def populate_fru_list(self):
         """ 
@@ -114,11 +231,34 @@ class MTCACrate():
             for line in result.splitlines():
                 try:
                     name, ref, status, id, desc = line.split('|')
-                    self.frus.append(FRU(name=name.strip(), id=id.strip()))
+                    # Get the AMC slot number
+                    slot = int(id.strip().split('.')[1])
+                    slot -= AMC_SLOT_1
+                    self.frus[slot] = FRU(
+                            name=name.strip(), 
+                            id=id.strip(), 
+                            slot=slot, 
+                            crate = self)
+                    self.frus_inited = True
                 except ValueError:
                     print "Couldn't parse {}".format(line)
         else:
             print("Crate information not populated")
+
+    def read_sensors(self):
+        """ 
+        Call read all sensor values
+
+        Args:   
+            None
+
+        Returns:
+            Nothing
+        """
+
+        for slot in self.frus:
+            self.frus[slot].read_sensors()
+
 
 _crate = MTCACrate()
 
@@ -133,21 +273,28 @@ class MTCACrateReader():
 
         Args:
             rec: pyDevSup record object
-            args: arguments from EPICS record
-                fn: function to be called
+            fn (str): function to be called
+            args (str): arguments from EPICS record
+                slot (int, optional): amc slot number 
+                sensor(str, optional): sensor to read
 
         Returns:
             Nothing
         """
 
-        fn = args
-	self.crate = get_crate()
-        self.process = getattr(self, fn)
+        args_list = args.split(None, 2)
+        if len(args_list) == 3:
+            fn, slot, sensor = args_list
+        else:
+            fn = args
+            slot = 0
+            sensor = None
 
-        try:
-            rec.UDF = 0
-        except AttributeError:
-            pass
+	    self.crate = get_crate()
+        self.process = getattr(self, fn)
+        self.slot = int(slot)
+        self.sensor = sensor
+        self.alarms_set = False
 
     def detach(self, rec):
         pass
@@ -163,6 +310,7 @@ class MTCACrateReader():
             Nothing
         """
         self.crate.host = rec.VAL
+        rec.UDF = 0
 
     def set_user(self, rec, report):
         """
@@ -175,6 +323,7 @@ class MTCACrateReader():
             Nothing
         """
         self.crate.user = rec.VAL
+        rec.UDF = 0
 
     def set_password(self, rec, report):
         """
@@ -187,6 +336,7 @@ class MTCACrateReader():
             Nothing
         """
         self.crate.password = rec.VAL
+        rec.UDF = 0
 
     def get_fru_list(self, rec, report):
         """
@@ -199,6 +349,47 @@ class MTCACrateReader():
             Nothing
         """
         self.crate.populate_fru_list()
+        rec.UDF = 0
+
+    def read_values(self, rec, report):
+        """
+        Read all sensor values for this crate
+
+        Args:
+            rec: pyDevSup record object
+
+        Returns:
+            Nothing
+        """
+
+
+    def get_val(self, rec, report):
+        """ 
+        Get sensor reading
+
+        Args:
+            rec: pyDevSup record object
+
+        Returns:
+            Nothing
+        """
+
+        if not self.alarms_set:
+            set_alarms(rec)
+
+        rec.VAL = getattr(self.crate.frus[self.slot], self.sensor)
+        rec.UDF = 0
+
+    def set_alarms(self, rec):
+        rec.LOLO = self.crate.frus[self.slot].sensors[self.sensor].lolo
+        rec.LOW = self.crate.frus[self.slot].sensors[self.sensor].low
+        rec.HIGH = self.crate.frus[self.slot].sensors[self.sensor].high
+        rec.HIHI = self.crate.frus[self.slot].sensors[self.sensor].hihi
+        rec.LLSV = 2 # MAJOR
+        rec.LSV = 1 # MINOR
+        rec.HSV = 1 # MINOR
+        rec.HHSV = 2 # MAJOR
+        self.alarms_set = True
 
 build = MTCACrateReader
 
