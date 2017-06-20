d1ddb4313f21c350f26816c8e90945a39749dd5f
Wayne Lewis <wlewis@ospreydcs.com>
2017-06-20 08:00:52 -0700

    Load correct databases

--- iocBoot/iocmtcaSensors/st.cmd
+++ iocBoot/iocmtcaSensors/st.cmd
@@ -14,10 +14,12 @@ dbLoadDatabase "dbd/mtcaSensors.dbd"
 mtcaSensors_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-dbLoadRecords("db/mtca_crate.db","P=$(CRATE_MCH)")
-dbLoadRecords("db/schroff_utca_cu.db","P=$(CRATE_MCH)30_97:,FRU_ID=30.97")
-dbLoadRecords("db/fgpdb.db","P=$(CRATE_MCH)193_102:,FRU_ID=193.102")
-dbLoadRecords("db/sis8300.db","P=$(CRATE_MCH)193_101:,FRU_ID=193.101")
+#dbLoadRecords("db/mtca_crate.db","P=$(CRATE_MCH)")
+#dbLoadRecords("db/schroff_utca_cu.db","P=$(CRATE_MCH)30_97:,FRU_ID=30.97")
+#dbLoadRecords("db/fgpdb.db","P=$(CRATE_MCH)193_102:,FRU_ID=193.102")
+#dbLoadRecords("db/sis8300.db","P=$(CRATE_MCH)193_101:,FRU_ID=193.101")
+dbLoadRecords("db/amc_cards.db")
+
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
