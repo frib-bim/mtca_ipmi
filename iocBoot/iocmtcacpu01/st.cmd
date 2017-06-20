7316a177d81bbb6cf3c9007c91670380e9f97a54
Wayne Lewis <wlewis@ospreydcs.com>
2017-06-20 19:54:25 -0400

    Set up startup script for MTCACPU01

--- iocBoot/iocmtcacpu01/st.cmd
+++ iocBoot/iocmtcacpu01/st.cmd
@@ -3,7 +3,8 @@
 ## You may have to change mtcaSensors to something else
 ## everywhere it appears in this file
 
-epicsEnvSet("CRATE", "MTCAMCH04:")
+epicsEnvSet("CRATE", "MTCAMCH03:")
+epicsEnvSet("MCH_HOST", "mtcamch03")
 
 < envPaths
 
@@ -14,10 +15,7 @@ dbLoadDatabase "dbd/mtcaSensors.dbd"
 mtcaSensors_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-dbLoadRecords("db/mtca_crate.db","P=$(CRATE)")
-#dbLoadRecords("db/schroff_utca_cu.db","P=$(CRATE)30_97:,FRU_ID=30.97")
-#dbLoadRecords("db/fgpdb.db","P=$(CRATE)193_102:,FRU_ID=193.102")
-#dbLoadRecords("db/sis8300.db","P=$(CRATE)193_101:,FRU_ID=193.101")
+dbLoadRecords("db/mtca_crate.db","P=$(CRATE),MCH_HOST=$(MCH_HOST)")
 dbLoadRecords("db/amc_cards.db","P=$(CRATE)")
 
 
