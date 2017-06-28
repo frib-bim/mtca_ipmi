25f73901cc6dfff19eeab52f0bc133b54916d240
Wayne Lewis <wlewis@ospreydcs.com>
2017-06-28 11:18:21 -0400

    DIAG-40 Add PVs for crate and rack ID

--- iocBoot/ioc-mtca01/st.cmd
+++ iocBoot/ioc-mtca01/st.cmd
@@ -5,6 +5,8 @@
 
 epicsEnvSet("CRATE", "MTCAMCH03:")
 epicsEnvSet("MCH_HOST", "mtcamch03")
+epicsEnvSet("CRATE_ID", "MTCA 03")
+epicsEnvSet("RACK_ID", "FE_N0404")
 
 < envPaths
 
@@ -15,7 +17,7 @@ dbLoadDatabase "dbd/mtcaSensors.dbd"
 mtcaSensors_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-dbLoadRecords("db/mtca_crate.db","P=$(CRATE),MCH_HOST=$(MCH_HOST)")
+dbLoadRecords("db/mtca_crate.db","P=$(CRATE),MCH_HOST=$(MCH_HOST),CRATE_ID=$(CRATE_ID),RACK_ID=$(RACK_ID)")
 dbLoadRecords("db/amc_cards.db","P=$(CRATE)")
 dbLoadRecords("db/cooling_units.db","P=$(CRATE)")
 dbLoadRecords("db/power_modules.db","P=$(CRATE)")
@@ -24,3 +26,4 @@ dbLoadRecords("db/mch.db","P=$(CRATE)")
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
+# This IOC can take >20 s to exit. Please be patient.
