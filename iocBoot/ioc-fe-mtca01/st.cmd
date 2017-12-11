5d73bc74bd020220560d91c1402516af11d444f1
Omitto <omitto@frib.msu.edu>
2017-12-11 14:38:15 -0500

    Fix mtca01 pv names

--- iocBoot/ioc-fe-mtca01/st.cmd
+++ iocBoot/ioc-fe-mtca01/st.cmd
@@ -3,7 +3,7 @@
 ## You may have to change mtcaSensors to something else
 ## everywhere it appears in this file
 
-epicsEnvSet("CRATE", "FE_MTCA01:MCH_N0102:")
+epicsEnvSet("CRATE", "FE_MTCA01:MCH_N0001:")
 epicsEnvSet("MCH_HOST", "fe-mtca01-mch-n0102")
 epicsEnvSet("CRATE_ID", "FE MTCA 01")
 epicsEnvSet("RACK_ID", "FE-001.06")
