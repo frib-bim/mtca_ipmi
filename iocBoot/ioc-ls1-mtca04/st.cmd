470e70a26007352c3c1104c7d452a3b4fdbb7285
Omitto <omitto@frib.msu.edu>
2017-12-11 14:35:58 -0500

    Fix mtca04 pv names

--- iocBoot/ioc-ls1-mtca04/st.cmd
+++ iocBoot/ioc-ls1-mtca04/st.cmd
@@ -3,10 +3,10 @@
 ## You may have to change mtcaSensors to something else
 ## everywhere it appears in this file
 
-epicsEnvSet("CRATE", "LS1_MTCA04:MCH_N0002:")
-epicsEnvSet("MCH_HOST", "ls1-mtca04-mch-n0002")
+epicsEnvSet("CRATE", "LS1_MTCA04:MCH_N0004:")
+epicsEnvSet("MCH_HOST", "ls1-mtca04-mch-n0004")
 epicsEnvSet("CRATE_ID", "LS1 MTCA 04")
-epicsEnvSet("RACK_ID", "LS1-002.06")
+epicsEnvSet("RACK_ID", "LS1-006.04")
 
 < envPaths
 
