8a17deccf3c76e2e44365f38dec226e772434bab
Wayne Lewis <wlewis@ospreydcs.com>
2018-01-31 17:13:09 -0500

    DIAG-92 Add card status calculation

--- iocBoot/ioc-mtca-common/st_mtca_common.cmd
+++ iocBoot/ioc-mtca-common/st_mtca_common.cmd
@@ -14,12 +14,13 @@ mtcaSensors_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
 # Set CU1, CU2 environment variables to override default cooling unit names.
+# Set PM environment variables to override default power module name.
 dbLoadRecords("db/mtca_crate.db","P=$(CRATE),MCH_HOST=$(MCH_HOST),CRATE_ID=$(CRATE_ID),RACK_ID=$(RACK_ID),CU1=$(CU1=CU01:),CU2=$(CU2=CU02:)")
-dbLoadRecords("db/amc_cards.db","P=$(CRATE)")
+dbLoadRecords("db/amc_cards.db","P=$(CRATE),PM=$(PM=PM02:)")
 dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU1=CU01:),UNIT=1")
 dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU2=CU02:),UNIT=2")
 dbLoadRecords("db/power_modules.db","P=$(CRATE)")
-dbLoadRecords("db/mch.db","P=$(CRATE)")
+dbLoadRecords("db/mch.db","P=$(CRATE),PM=$(PM=PM02:)")
 dbLoadRecords("db/save_restoreStatus.db","P=$(CRATE)")
 
 # Autosave setup
