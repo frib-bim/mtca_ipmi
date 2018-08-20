9c8b53f0218f02371404d5afde56e8206286e06a
Bruno Martins <martins@frib.msu.edu>
2018-08-20 17:54:26 -0400

    Standardize

--- iocBoot/ioc-mtca-common/st_mtca_common.cmd
+++ iocBoot/ioc-mtca-common/st_mtca_common.cmd
@@ -4,8 +4,6 @@
 # < st_mtca_common.cmd
 # after setting the environment variables.
 
-epicsEnvSet("AS_PATH", "$(TOP)/as/$(IOC)")
-
 cd $(TOP)
 
 ## Register all support components
@@ -21,26 +19,8 @@ dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU1=CU01:),UNIT=1")
 dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU2=CU02:),UNIT=2")
 dbLoadRecords("db/power_modules.db","P=$(CRATE)")
 dbLoadRecords("db/mch.db","P=$(CRATE),PM=$(PM=PM02:)")
-dbLoadRecords("db/save_restoreStatus.db","P=$(CRATE)")
-
-# Autosave setup
-save_restoreSet_Debug(0)
-save_restoreSet_IncompleteSetsOk(1)
-save_restoreSet_DatedBackupFiles(1)
-save_restoreSet_status_prefix("$(CRATE)")
-
-set_savefile_path("$(AS_PATH)","/sav")
-
-set_pass0_restoreFile("info_settings.sav")
-set_pass1_restoreFile("info_settings.sav")
 
 iocInit()
-iocLogInit()
-caPutLogInit("${EPICS_PUT_LOG_INET}:${EPICS_PUT_LOG_PORT}", 1)
-
-cd $(AS_PATH)/req
-makeAutosaveFiles()
-create_monitor_set("info_settings.req", 15)
 
 cd $(TOP)
 
